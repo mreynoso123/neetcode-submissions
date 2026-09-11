d93915af6ec0706b18c7c21f2d07713055704c8a
mreynoso123 <131721792+mreynoso123@users.noreply.github.com>
2026-09-11 10:34:00 -0700

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,13 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+print(your_dict) # print the dictionary
+print(your_dict['a']) # print the value at key a
+print('d' in your_dict) # print boolean if d exists
+
+your_dict['a'] = 4 # reassigns value at a
+
+print(your_dict) # print the dictionary
